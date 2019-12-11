ae0376a59d1f677995e2b5bd0d0661160e80248b
tomoberu <57937124+tomoberu@users.noreply.github.com>
2019-12-11 16:40:03 +0900

    model-branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+        
+  has_many :posts
+  has_many :category_users
+  has_many :category, through: :category_users
 end
